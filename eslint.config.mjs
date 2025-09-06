e9309d1f0547a76f086c928e693f137407c9af19
abdelbassetbenatmani <abdelbassetbenatmani@gmail.com>
2025-09-06 13:31:07 +0100

    chore: update dependencies and devDependencies in package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    // off any
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
